9f57680b69659e18e6a9eb0873d1d6d76c16e088
箱崎章朗 <hakozakifumiaki@hakozaki-fumiaki-MacBook-Air.local>
2022-12-05 19:02:46 +0900

    演習2

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,34 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def new
     @contact = Contact.new
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to contacts_path, notice: "削除しました！"
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(contact_params)
+      redirect_to contacts_path, notice: "内容変更しました！"
+      else
+        render :edit
+
+    end
   end
 
   def create
@@ -11,10 +36,15 @@ class ContactsController < ApplicationController
     #Contact.create(params.require(:contact).permit(:Name, :Email, :Content))
     @contact = Contact.new(Name: params[:contact][:Name],Email: params[:contact][:Email],Content: params[:contact][:content])
     if @contact.save
-      redirect_to contacts_path, notice: "作成しました！"
+      redirect_to new_contact_path, notice: "作成しました！"
     else
       render :new
     end
+  end
+  private
+
+  def contact_params
+    params.require(:contact).permit(:Name, :Email, :Content)
   end
 end
 
